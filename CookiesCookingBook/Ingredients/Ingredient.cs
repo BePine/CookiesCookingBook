14a965a0686e1b338fb21cf57ab0f0cbd2653e00
Cyprian Tom <bepine17@gmail.com>
2024-05-29 22:38:21 +0200

    Added a little bittle of fields

--- CookiesCookingBook/Ingredients/Ingredient.cs
+++ CookiesCookingBook/Ingredients/Ingredient.cs
@@ -1,76 +1,117 @@
-﻿namespace Ingredient
+﻿namespace Ingredients.Ingredient
 {
+
+    
     public abstract class Ingredient
     {
         public virtual int id { get; } = 0;
         public virtual string name { get; } = "ingredient";
         public virtual string instructions { get; } = "instructions";
+        public static List<Ingredient> ingredients = new List<Ingredient>();
         public virtual void Show()
         {
             Console.WriteLine($"{id}. {name}. {instructions}");
         }
-        public static List<Ingredient> ConvertIngredientsToList()
-        {
-            List<Ingredient> result = new List<Ingredient>();   
-
-            return result;
+        public void AddThisIngredientToTheList()
+        { 
+            
+            ingredients.Add( this );
         }
+
+        
     }
 
     public class WhiteSugar : Ingredient
     {
+        public WhiteSugar()
+        {
+            this.AddThisIngredientToTheList();
+        }
         public override int id { get; } = 1;
         public override string name { get; } = "White Sugar";
         public override string instructions { get; } = "Add 2 spoons to other ingredients.";
     }
     public class BrownSugar : Ingredient
     {
+        public BrownSugar()
+        {
+            this.AddThisIngredientToTheList();
+        }
         public override int id { get; } = 2;
         public override string name { get; } = "White Sugar";
         public override string instructions { get; } = "Add 2 spoons to other ingredients.";
     }
     public class CocoaPowder : Ingredient
     {
+        public CocoaPowder()
+        {
+            this.AddThisIngredientToTheList();
+        }
         public override int id { get; } = 3;
         public override string name { get;  } = "White Sugar";
         public override string instructions { get;  } = "Add 2 spoons to other ingredients.";
     }
     public class Flour : Ingredient
     {
+        public Flour()
+        {
+            this.AddThisIngredientToTheList();
+        }
         public override int id { get; } = 4;
         public override string name { get; } = "White Sugar";
         public override string instructions { get; } = "Add 2 spoons to other ingredients.";
     }
     public class Milk : Ingredient
     {
+        public Milk()
+        {
+            this.AddThisIngredientToTheList();
+        }
         public override int id { get; } = 5;
         public override string name { get; } = "White Sugar";
         public override string instructions { get; } = "Add 2 spoons to other ingredients.";
     }
     public class Cinnamon : Ingredient
     {
+        public Cinnamon()
+        {
+            this.AddThisIngredientToTheList();
+
+        }
         public override int id { get; } = 6;
         public override string name { get; } = "White Sugar";
         public override string instructions { get; } = "Add 2 spoons to other ingredients.";
     }
     public class Butter : Ingredient
     {
+        public Butter()
+        {
+            this.AddThisIngredientToTheList();
+
+        }
         public override int id { get; } = 7;
         public override string name { get; } = "White Sugar";
         public override string instructions { get; } = "Add 2 spoons to other ingredients.";
     }
+    
     public class Chocolate : Ingredient
     {
+        public Chocolate()
+        {
+            this.AddThisIngredientToTheList();
+
+        }
         public override int id { get; } = 8;
         public override string name { get; } = "White Sugar";
         public override string instructions { get; } = "Add 2 spoons to other ingredients.";
     }
+    public class CreateIngredients
+    {
+        public void CreateIngredientsInstances()
+        {
+
+        }
+    }
 
 }
-public class ShowIngredients
-{
-    public void PrintAllIngredients()
-    {
-        for (int i = 0; i < 10; i++) { }
-    }
-}
+
--- CookiesCookingBook/Main.cs
+++ CookiesCookingBook/Main.cs
@@ -1,5 +1,10 @@
-﻿using Ingredient;
+﻿using Ingredients.Ingredient;
+
 WhiteSugar whiteSugar = new WhiteSugar();
 whiteSugar.Show();
+for(int i=0; i > Ingredient.ingredients.Count; i++)
+{
+    Console.WriteLine($"{Ingredient.ingredients[i].id}. {Ingredient.ingredients[i].name}");
+}
 Console.ReadKey();
 
