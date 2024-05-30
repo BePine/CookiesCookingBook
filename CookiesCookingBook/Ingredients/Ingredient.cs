591563e6ea262d93c8ebe72d0e742c1fd01d315d
Cyprian Tom <bepine17@gmail.com>
2024-05-30 17:28:21 +0200

    Finally managed to create somewhat satisfying Ingredients and their features as displaying etc.

--- CookiesCookingBook/Ingredients/Ingredient.cs
+++ CookiesCookingBook/Ingredients/Ingredient.cs
@@ -12,12 +12,22 @@
         {
             Console.WriteLine($"{id}. {name}. {instructions}");
         }
+        public static void CreateIngredients()
+        {
+            new WhiteSugar();
+            new BrownSugar();
+            new CocoaPowder();
+            new Flour();
+            new Milk();
+            new Cinnamon();
+            new Butter();
+            new Chocolate();
+        }
         public void AddThisIngredientToTheList()
         { 
-            
             ingredients.Add( this );
         }
-
+       
         
     }
 
@@ -29,7 +39,7 @@
         }
         public override int id { get; } = 1;
         public override string name { get; } = "White Sugar";
-        public override string instructions { get; } = "Add 2 spoons to other ingredients.";
+        public override string instructions { get; } = "Add 2 spoons to the other ingredients.";
     }
     public class BrownSugar : Ingredient
     {
@@ -38,8 +48,8 @@
             this.AddThisIngredientToTheList();
         }
         public override int id { get; } = 2;
-        public override string name { get; } = "White Sugar";
-        public override string instructions { get; } = "Add 2 spoons to other ingredients.";
+        public override string name { get; } = "Brown Sugar";
+        public override string instructions { get; } = "Add 1.5 spoons to the other ingredients.";
     }
     public class CocoaPowder : Ingredient
     {
@@ -48,8 +58,8 @@
             this.AddThisIngredientToTheList();
         }
         public override int id { get; } = 3;
-        public override string name { get;  } = "White Sugar";
-        public override string instructions { get;  } = "Add 2 spoons to other ingredients.";
+        public override string name { get;  } = "Cocoa Powder";
+        public override string instructions { get;  } = "Add 2.5 spoon to the other ingredients.";
     }
     public class Flour : Ingredient
     {
@@ -58,8 +68,8 @@
             this.AddThisIngredientToTheList();
         }
         public override int id { get; } = 4;
-        public override string name { get; } = "White Sugar";
-        public override string instructions { get; } = "Add 2 spoons to other ingredients.";
+        public override string name { get; } = "Flour";
+        public override string instructions { get; } = "Add 3 spoons to the other ingredients.";
     }
     public class Milk : Ingredient
     {
@@ -68,8 +78,8 @@
             this.AddThisIngredientToTheList();
         }
         public override int id { get; } = 5;
-        public override string name { get; } = "White Sugar";
-        public override string instructions { get; } = "Add 2 spoons to other ingredients.";
+        public override string name { get; } = "Milk";
+        public override string instructions { get; } = "Add half a glass.";
     }
     public class Cinnamon : Ingredient
     {
@@ -79,8 +89,8 @@
 
         }
         public override int id { get; } = 6;
-        public override string name { get; } = "White Sugar";
-        public override string instructions { get; } = "Add 2 spoons to other ingredients.";
+        public override string name { get; } = "Cinnamon";
+        public override string instructions { get; } = "Add half of spoon to the other ingredients.";
     }
     public class Butter : Ingredient
     {
@@ -90,8 +100,8 @@
 
         }
         public override int id { get; } = 7;
-        public override string name { get; } = "White Sugar";
-        public override string instructions { get; } = "Add 2 spoons to other ingredients.";
+        public override string name { get; } = "Butter";
+        public override string instructions { get; } = "Add half of spoon to the other ingredients.";
     }
     
     public class Chocolate : Ingredient
@@ -102,16 +112,8 @@
 
         }
         public override int id { get; } = 8;
-        public override string name { get; } = "White Sugar";
-        public override string instructions { get; } = "Add 2 spoons to other ingredients.";
+        public override string name { get; } = "Chocolate";
+        public override string instructions { get; } = "Add around 6 pieces of chocolate to the rest of the ingredients.";
     }
-    public class CreateIngredients
-    {
-        public void CreateIngredientsInstances()
-        {
-
-        }
-    }
-
 }
 
--- CookiesCookingBook/Main.cs
+++ CookiesCookingBook/Main.cs
@@ -1,10 +1,11 @@
 ﻿using Ingredients.Ingredient;
 
-WhiteSugar whiteSugar = new WhiteSugar();
-whiteSugar.Show();
-for(int i=0; i > Ingredient.ingredients.Count; i++)
+
+Ingredient.CreateIngredients();
+for(int i=0; i < Ingredient.ingredients.Count; i++)
 {
     Console.WriteLine($"{Ingredient.ingredients[i].id}. {Ingredient.ingredients[i].name}");
 }
+
 Console.ReadKey();
 
