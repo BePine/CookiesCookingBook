2282a5eed9708f37b5569455c5c3070c8c5ffbef
BePine <BePine17@gmail.com>
2024-06-09 22:38:31 +0200

    Added global try catch

--- CookiesCookingBook/Main.cs
+++ CookiesCookingBook/Main.cs
@@ -2,17 +2,24 @@ using CookiesCookingBook.App;
 using CookiesCookingBook.Recipes;
 using CookiesCookingBook.Recipes.DataAccess;
 using CookiesCookingBook.Recipes.Ingredients;
+try
+{
+    var ingredientsRegister = new IngredientsRegister();
+    var cookingBookApp = new CookingBookApp(
+        new RecipesRepository(
+            new StringsTextualRepository(),
+            ingredientsRegister),
+        new RecipesConsoleUserInteraction(
+            ingredientsRegister));
 
-var ingredientsRegister = new IngredientsRegister();
-var cookingBookApp = new CookingBookApp(
-    new RecipesRepository(
-        new StringsTextualRepository(),
-        ingredientsRegister),
-    new RecipesConsoleUserInteraction(
-        ingredientsRegister));
-
-cookingBookApp.run("recipes.txt");
-
+    cookingBookApp.run("recipes.txt");
+}
+catch(Exception ex)
+{
+    Console.WriteLine("Sorry, the application encountered an unexpected error and will be closed." + ex.Message);
+    Console.WriteLine("Press any key to close.");
+    Console.ReadKey();
+}
 
 
 
